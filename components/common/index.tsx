9d074578e38ff762e0a86baf5560cd8226e121e2
caarbelaezgP202 <carlos.arbelaez@projekt202.com>
2023-08-03 10:23:36 -0500

    Folder restructure 2

---
+++ components/common/index.tsx
@@ -0,0 +1,7 @@
+import Subheading from './Subheading';
+import TextField from './TextField';
+
+export {
+  Subheading,
+  TextField
+};
--- components/pages/Home/index.tsx
+++ components/pages/Home/index.tsx
@@ -5,7 +5,7 @@ import { AnimatePresence, motion } from 'framer-motion';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
-import Subheading from '../../Subheading';
+import { Subheading } from '@/components/common';
 
 
 const HomePage = () => {
